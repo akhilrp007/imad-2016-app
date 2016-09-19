ed26fec942a712d1acb5dd5ac43bc3ff1b20959f
akhilrp007 <akhilrp221093@gmail.com>
2016-09-19 10:00:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@
        res.sendFile(path.join(__dirname, 'ui', 'index.html'));
      });
      
-     app.get('/articel-one',function(req,res){
+     app.get('/article-one',function(req,res){
          res.send('Article one req');
      });
      
-     app.get('/articel-two',function(req,res){
+     app.get('/article-two',function(req,res){
          res.send('Article two req');
      });
      
-     app.get('/articel-three',function(req,res){
+     app.get('/article-three',function(req,res){
          res.send('Article three req');
      });
      
