a6dae69a6bee251ef8bbaac070b959c2c840d6ee
akhilrp007 <akhilrp221093@gmail.com>
2016-09-19 09:53:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,8 @@
        res.sendFile(path.join(__dirname, 'ui', 'index.html'));
      });
      
+     app.get()
+     
      app.get('/ui/style.css', function (req, res) {
       res.sendFile(path.join(__dirname, 'ui', 'style.css'));
      });
