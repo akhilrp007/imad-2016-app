56777c9c4c628868727da48f8c87004a9b50f9bf
akhilrp007 <akhilrp221093@gmail.com>
2016-10-09 22:55:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,33 +1,38 @@
-<html>
-    <head>
-        <title>
-            Article one 
-        </title>
-        
-    </head>
-    <body>
-        <div>
-            <a href="/">Home</a>
-        </div>
-        
-        <hr/>
-        <h3>
-            Article One
-        </h3>
-        <div>
-            Oct 9 2016
-        </div>
-        <div>
-            <p>
-                This is first time in html.  This is first  in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is
-            </p>
-            <p>
-                 This is first time in html.  This is first  in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is 
-
-            </p>
-            <p>
-                This is first time in html.  This is first  in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is 
-            </p>
-        </div>
-    </body>
-</html>
+    var express = require('express');
+     var morgan = require('morgan');
+     var path = require('path');
+     
+     var app = express();
+     app.use(morgan('combined'));
+     
+    app.get('/', function (req, res) {
+       res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+     });
+     
+     app.get('/article-one',function(req,res){
+         res.send('Article one req');
+     });
+     
+     app.get('/article-two',function(req,res){
+         res.send('Article two req');
+     });
+     
+     app.get('/article-three',function(req,res){
+         res.send('Article three req');
+     });
+     
+     
+     
+     app.get('/ui/style.css', function (req, res) {
+      res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+     });
+     
+     app.get('/ui/madi.png', function (req, res) {
+       res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+     });
+     
+     
+     var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+     app.listen(8080, function () {
+      console.log(`IMAD course app listening on port ${port}!`);
+     });
