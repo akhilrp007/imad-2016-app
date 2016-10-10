c7c16fbc5b8c5ed4fd86c8a552822330232aa7c0
akhilrp007 <akhilrp221093@gmail.com>
2016-10-10 21:32:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,14 +8,64 @@
      
      var content = {
          title:'article-one',
+         heading:'article-one',
+         date:'oct 9 2016',
+         content:` <p>
+                       This is first time in html.  This is first  in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is
+                    </p>
+                    <p>
+                       This is first time in html.  This is first  in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is 
+        
+                    </p>
+                    <p>
+                        This is first time in html.  This is first  in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is first time in html.  This is 
+                    </p>`
          
+     };
+     function createTemplate(data){
+         var title= data.title;
+         var date= data.date;
+         var heading=data.heading;
+         var content=data.content;
+         var htmltemplate = `
+         <html>
+        <head>
+            <title>
+                $(title) 
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+           <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                    <div>
+                         <a href="/">Home</a>
+                    </div>
+                    
+                    <hr/>
+                    <h3>
+                       $(heading)
+                    </h3>
+                    <div>
+                        $(date)
+                    </div>
+                    <div>
+                        $(content)
+                    </div>
+                </div>
+        </body>
+    </html>
+     `;
+     return htmltemplate;
      }
+     
+     
     app.get('/', function (req, res) {
        res.sendFile(path.join(__dirname, 'ui', 'index.html'));
      });
      
      app.get('/article-one',function(req,res){
-         res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+         res.send(createTemplate(articleone));
      });
      
      app.get('/article-two',function(req,res){
