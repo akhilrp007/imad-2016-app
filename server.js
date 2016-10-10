f84eaa6e8d3c307e9b9a094e7b2f993e89ca4253
akhilrp007 <akhilrp221093@gmail.com>
2016-10-10 21:47:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@
          <html>
         <head>
             <title>
-                $(title) 
+                ${title} 
             </title>
             <meta name="viewport" content="width=device-width, initial-scale=1">
            <link href="/ui/style.css" rel="stylesheet" />
