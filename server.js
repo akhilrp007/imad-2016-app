f0ed8ef38a24231381b930c7d5404157cd8a0df5
akhilrp007 <akhilrp221093@gmail.com>
2016-10-11 23:46:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,7 +131,7 @@
      });
      
      var names = [];
-     app.get('/submit-name/' function (req,res){
+     app.get('/submit-name' function (req,res){
          var name = req.query.name;
          names.push(name);
          res.send(JSON.stringify(names));
