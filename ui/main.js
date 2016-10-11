30a0d7eced10eeeed9b4477631f93b50256b28bf
akhilrp007 <akhilrp221093@gmail.com>
2016-10-11 23:23:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,19 @@ button.onclick = function() {
     request.open('GET','http://akhilrp007.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+//submitname
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('sub');
+submit.onclick=function() 
+{
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for(var i=0 , i<names.length , i++){
+        list+='<li>'+names[i]+'</li>'
+        
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML = list;
+};
