508332fea44865dfc3da648d90b4737634cb1e60
akhilrp007 <akhilrp221093@gmail.com>
2016-10-11 12:46:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,6 +115,11 @@
       res.sendFile(path.join(__dirname, 'ui', 'style.css'));
      });
      
+     app.get('/ui/main.js', function (req, res) {
+      res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+     });
+     
+     
      app.get('/ui/madi.png', function (req, res) {
        res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
      });
