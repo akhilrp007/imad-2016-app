1eaf2fd046e7a9150e9aee8eecb8a5297bb32325
akhilrp007 <akhilrp221093@gmail.com>
2016-10-11 20:48:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,7 @@
-console.log('Loaded!');
-var element = document.getElementById('main');
-element.innerHTML='New ca';
- 
- 
- var img = document.getElementById('main');
- 
- var img = document.getElementById('madi');
- var marginLeft=0;
-function moveRight(){
-     marginLeft=marginLeft+10;
-     img.style.marginLeft=marginLeft+'px';
- }
- img.onclick=function(){
-     //img.style.marginLeft='100px';
-     var interval = setInterval(moveRight,100);
- };
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+  counter = counter+1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+};
