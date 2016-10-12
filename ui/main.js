1712c8139fa0cb249273d0acb83afa84f31010a6
akhilrp007 <akhilrp221093@gmail.com>
2016-10-12 11:03:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,7 +48,8 @@ submit.onclick=function()
                 }
                 var ul=document.getElementById('namelist');
                 ul.innerHTML = list;
-             
+            
+              span.innerHTML 
           }
       }
   };
