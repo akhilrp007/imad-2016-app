18705f445dfb863f93d1c544d0b3b20705609b39
akhilrp007 <akhilrp221093@gmail.com>
2016-10-12 10:47:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,12 +30,33 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick=function() 
 {
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0; i<names.length; i++){
-        list+='<li>'+names[i]+'</li>';
-        
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML = list;
+         var request = new XMLHttpRequest();
+    
+    
+    //capture the response and store it in a variable
+    
+         request.onreadystatechange = function() {
+         if(request.readyState === XMLHttpRequest.DONE)  {
+          
+          
+         if (request.status === 200){
+              var names = ['name1','name2','name3','name4'];
+               var list = '';
+                 for(var i=0; i<names.length; i++){
+                    list+='<li>'+names[i]+'</li>';
+                    
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML = list;
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }
+  };
+    
+    //make request
+    request.open('GET','http://akhilrp007.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+   
 };
