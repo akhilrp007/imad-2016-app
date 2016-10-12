66c958a0eddea769ce732a987d36330bce0277fb
akhilrp007 <akhilrp221093@gmail.com>
2016-10-12 10:59:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,7 @@ button.onclick = function() {
 };
 //submitname
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick=function() 
 {
